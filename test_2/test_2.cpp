f90ef8213b3108a6b0bffd9fe5b6e70e4ce7e4e7
25949 <25949@Z133>
2023-11-08 11:36:56 +0100

    Dodaj pliki projektów.

---
+++ test_2/test_2.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+using namespace std;
+int i,w,j,k;
+int main()
+{
+    cout << "podaj ilość wierszy\n";
+    cin >> w;
+
+    for (i = 0; i < w; i++) {
+        cout << "* ";
+    }
+    cout << "podaj ilość kolumn\n";
+    cin >> k;
+        for (j = 0; j < k; j++) {
+            cout << ". ";
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+#include <iostream>
+
+using namespace std;
+
+void drukujLinie(int);
+void drukujLinie2(int);
+void drukujSzachownice(int);
+int main()
+{
+    int szerokosc;
+    cout << "Podaj szerokosc szachownicy: ";
+    cin >> szerokosc;
+    drukujSzachownice(szerokosc);
+
+}
+void drukujSzachownice(int x)
+{
+    for (int i = 0; i < x / 2; i++)
+    {
+        drukujLinie(x);
+        drukujLinie2(x);
+    }
+}
+void drukujLinie(int y)
+{
+    for (int i = 1; i <= y / 2; i++)
+    {
+        cout << "*";
+    }
+    cout << endl;
+}
+void drukujLinie2(int z)
+{
+    for (int i = 1; i <= z / 2; i++)
+    {
+        cout << ".";
+    }
+    cout << endl;
+}
+*/
